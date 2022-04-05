e8a1996ed3450d3b6e5d9bab0540b13bb048f425
Nikola <nikoladevelops@gmail.com>
2022-04-05 11:40:25 +0300

    Working on the forms.

--- BelPomagalo/Views/AddNewEntityForms/AddNewGenreForm.Designer.cs
+++ BelPomagalo/Views/AddNewEntityForms/AddNewGenreForm.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.addNewGenreButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.genreNameTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // addNewGenreButton
+            // 
+            this.addNewGenreButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addNewGenreButton.Location = new System.Drawing.Point(28, 100);
+            this.addNewGenreButton.Name = "addNewGenreButton";
+            this.addNewGenreButton.Size = new System.Drawing.Size(295, 59);
+            this.addNewGenreButton.TabIndex = 5;
+            this.addNewGenreButton.Text = "Добави";
+            this.addNewGenreButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Име на жанра";
+            // 
+            // genreNameTextBox
+            // 
+            this.genreNameTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.genreNameTextBox.Location = new System.Drawing.Point(28, 50);
+            this.genreNameTextBox.Name = "genreNameTextBox";
+            this.genreNameTextBox.Size = new System.Drawing.Size(295, 35);
+            this.genreNameTextBox.TabIndex = 3;
             // 
             // AddNewGenreForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(348, 201);
+            this.Controls.Add(this.addNewGenreButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.genreNameTextBox);
             this.Name = "AddNewGenreForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddNewGenreForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button addNewGenreButton;
+        private Label label1;
+        private TextBox genreNameTextBox;
     }
 }
--- BelPomagalo/Views/AddNewEntityForms/AddNewThemeForm.Designer.cs
+++ BelPomagalo/Views/AddNewEntityForms/AddNewThemeForm.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.themeNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addNewThemeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // themeNameTextBox
+            // 
+            this.themeNameTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.themeNameTextBox.Location = new System.Drawing.Point(23, 54);
+            this.themeNameTextBox.Name = "themeNameTextBox";
+            this.themeNameTextBox.Size = new System.Drawing.Size(295, 35);
+            this.themeNameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Име на темата";
+            // 
+            // addNewThemeButton
+            // 
+            this.addNewThemeButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addNewThemeButton.Location = new System.Drawing.Point(23, 104);
+            this.addNewThemeButton.Name = "addNewThemeButton";
+            this.addNewThemeButton.Size = new System.Drawing.Size(295, 59);
+            this.addNewThemeButton.TabIndex = 2;
+            this.addNewThemeButton.Text = "Добави";
+            this.addNewThemeButton.UseVisualStyleBackColor = true;
             // 
             // AddNewThemeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(348, 201);
+            this.Controls.Add(this.addNewThemeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.themeNameTextBox);
             this.Name = "AddNewThemeForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddNewThemeForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox themeNameTextBox;
+        private Label label1;
+        private Button addNewThemeButton;
     }
 }
