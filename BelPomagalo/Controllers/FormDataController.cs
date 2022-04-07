4ad9a56db994e1947773cadb26718034a69d6020
Nikola <nikoladevelops@gmail.com>
2022-04-07 12:37:51 +0300

    Removed unused namespaces

--- BelPomagalo/Controllers/FormDataController.cs
+++ BelPomagalo/Controllers/FormDataController.cs
@@ -1,11 +1,6 @@
 ﻿using BelPomagalo.Models;
 using BelPomagalo.Services;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BelPomagalo.Controllers
 {
