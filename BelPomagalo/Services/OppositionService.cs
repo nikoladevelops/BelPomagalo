75c58c255a8191d2be89c42741a3ea3106389c09
Nikola <nikoladevelops@gmail.com>
2022-04-08 10:27:16 +0300

    Created OppositionService and PublishedWorkOppositionService

---
+++ BelPomagalo/Services/OppositionService.cs
@@ -0,0 +1,32 @@
+﻿using BelPomagalo.Models;
+namespace BelPomagalo.Services
+{
+    internal class OppositionService : Service
+    {
+        public OppositionService(ApplicationDbContext context) : base(context)
+        {
+        }
+        internal IEnumerable<string> GetAllOppositionsNames()
+        {
+            return _context.Oppositions
+                .Select(x => x.Name)
+                .ToList();
+        }
+        internal Opposition GetOpposition(string oppositionName)
+        {
+            return _context.Oppositions
+                .FirstOrDefault(x => x.Name == oppositionName);
+        }
+        internal Opposition GetOpposition(int oppositionId)
+        {
+            return _context.Oppositions
+                .FirstOrDefault(x => x.Id == oppositionId);
+        }
+        internal async Task<Opposition> AddOpposition(Opposition opposition)
+        {
+            var addedOpposition = await _context.Oppositions.AddAsync(opposition);
+            await _context.SaveChangesAsync();
+            return addedOpposition.Entity;
+        }
+    }
+}
---
+++ BelPomagalo/Services/PublishedWorkOppositionService.cs
@@ -0,0 +1,23 @@
+﻿using BelPomagalo.Models;
+
+namespace BelPomagalo.Services
+{
+    internal class PublishedWorkOppositionService : Service
+    {
+        public PublishedWorkOppositionService(ApplicationDbContext context) : base(context)
+        {
+        }
+        internal IEnumerable<PublishedWorkOpposition> GetPublishedWorksOppositions(PublishedWork publishedWork)
+        {
+            return _context.PublishedWorkOppositions
+                .Where(x => x.PublishedWorkId == publishedWork.Id)
+                .ToList();
+        }
+        internal async Task<PublishedWorkOpposition> AddPublishedWorkOpposition(PublishedWorkOpposition publishedWorkOpposition)
+        {
+            var result = await _context.PublishedWorkOppositions.AddAsync(publishedWorkOpposition);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+    }
+}
