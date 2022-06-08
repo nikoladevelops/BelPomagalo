ff3a8d68dbd193e046b93b20d225fe179cc668fa
Nikola <nikoladevelops@gmail.com>
2022-06-08 13:08:02 +0300

    Created AddNewQuestionChlenuvane form.

---
+++ BelPomagalo/Views/AddNewEntityForms/AddNewQuestionChlenuvane.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace BelPomagalo.Views.AddNewEntityForms
+{
+    partial class AddNewQuestionChlenuvane
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addNewAuthorButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sentenceTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.correctSentenceTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wrongAnswersTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.correctAnswersTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addNewAuthorButton
+            // 
+            this.addNewAuthorButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.addNewAuthorButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addNewAuthorButton.Location = new System.Drawing.Point(210, 286);
+            this.addNewAuthorButton.Name = "addNewAuthorButton";
+            this.addNewAuthorButton.Size = new System.Drawing.Size(295, 59);
+            this.addNewAuthorButton.TabIndex = 5;
+            this.addNewAuthorButton.Text = "Добави";
+            this.addNewAuthorButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(29, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Изречение - задача";
+            // 
+            // sentenceTextBox
+            // 
+            this.sentenceTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.sentenceTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sentenceTextBox.Location = new System.Drawing.Point(29, 47);
+            this.sentenceTextBox.Name = "sentenceTextBox";
+            this.sentenceTextBox.Size = new System.Drawing.Size(295, 35);
+            this.sentenceTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(362, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Правилно изречение";
+            // 
+            // correctSentenceTextBox
+            // 
+            this.correctSentenceTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.correctSentenceTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.correctSentenceTextBox.Location = new System.Drawing.Point(362, 47);
+            this.correctSentenceTextBox.Name = "correctSentenceTextBox";
+            this.correctSentenceTextBox.Size = new System.Drawing.Size(295, 35);
+            this.correctSentenceTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(362, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Грешни отговори";
+            // 
+            // wrongAnswersTextBox
+            // 
+            this.wrongAnswersTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.wrongAnswersTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.wrongAnswersTextBox.Location = new System.Drawing.Point(362, 132);
+            this.wrongAnswersTextBox.Name = "wrongAnswersTextBox";
+            this.wrongAnswersTextBox.Size = new System.Drawing.Size(295, 35);
+            this.wrongAnswersTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(29, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 25);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Верни отговори";
+            // 
+            // correctAnswersTextBox
+            // 
+            this.correctAnswersTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.correctAnswersTextBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.correctAnswersTextBox.Location = new System.Drawing.Point(29, 132);
+            this.correctAnswersTextBox.Name = "correctAnswersTextBox";
+            this.correctAnswersTextBox.Size = new System.Drawing.Size(295, 35);
+            this.correctAnswersTextBox.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(90, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(537, 25);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Всички отговори трябва да са отделени със запетая и място";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(90, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(288, 25);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Пример: човекът, художници, ...";
+            // 
+            // AddNewQuestionChlenuvane
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(166)))), ((int)(((byte)(131)))));
+            this.ClientSize = new System.Drawing.Size(687, 369);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wrongAnswersTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.correctAnswersTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.correctSentenceTextBox);
+            this.Controls.Add(this.addNewAuthorButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sentenceTextBox);
+            this.Name = "AddNewQuestionChlenuvane";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добави Задача За Членуване";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button addNewAuthorButton;
+        private Label label1;
+        private TextBox sentenceTextBox;
+        private Label label3;
+        private TextBox correctSentenceTextBox;
+        private Label label2;
+        private TextBox wrongAnswersTextBox;
+        private Label label4;
+        private TextBox correctAnswersTextBox;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ BelPomagalo/Views/AddNewEntityForms/AddNewQuestionChlenuvane.cs
@@ -0,0 +1,15 @@
+﻿namespace BelPomagalo.Views.AddNewEntityForms
+{
+    public partial class AddNewQuestionChlenuvane : Form, IAddForm
+    {
+        public AddNewQuestionChlenuvane()
+        {
+            InitializeComponent();
+        }
+        public TextBox SentenceTextBox { get => sentenceTextBox; }
+        public TextBox CorrectSentenceTextBox { get => correctSentenceTextBox; }
+        public TextBox CorrectAnswersTextBox { get => correctAnswersTextBox; }
+        public TextBox WrongAnswersTextBox { get => wrongAnswersTextBox; }
+        public Button AddButton { get => addNewAuthorButton; }
+    }
+}
